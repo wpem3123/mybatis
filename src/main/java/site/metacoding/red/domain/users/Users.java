5e738f91883ab59c63fcfe4e4c6d5ae02bba5e6e
wpem3123 <wpem3123@naver.com>
2022-09-01 14:54:40 +0900

    mybatis-Setting

---
+++ src/main/java/site/metacoding/red/domain/users/Users.java
@@ -0,0 +1,28 @@
+package site.metacoding.red.domain.users;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+import site.metacoding.red.web.dto.request.users.UpdateDto;
+
+
+@Getter
+public class Users {
+	private Integer id;
+	private String username;
+	private String password;
+	private String email;
+	private Timestamp createdAt;
+	
+	public void 패스워드수정(String password) {
+		this.password = password;
+	}
+	
+	public void 전체수정(UpdateDto updateDto) {
+		this.username = updateDto.getUsername();
+		this.password = updateDto.getPassword();
+		this.email = updateDto.getEmail();
+	}
+	
+}
---
+++ src/main/java/site/metacoding/red/web/UsersController.java
@@ -0,0 +1,71 @@
+package site.metacoding.red.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.red.domain.users.Users;
+import site.metacoding.red.domain.users.UsersDao;
+import site.metacoding.red.web.dto.request.users.JoinDto;
+import site.metacoding.red.web.dto.request.users.UpdateDto;
+import site.metacoding.red.web.dto.response.RespDto;
+
+
+@RequiredArgsConstructor
+@RestController
+public class UsersController {
+
+	private final UsersDao usersDao;
+	
+	
+	
+	@GetMapping("/users/{id}")
+	public RespDto<?> getUsers(@PathVariable Integer id) {
+		
+		
+		return new RespDto<>(1,"성공",usersDao.findById(id));
+	}
+	
+	@GetMapping("/users")
+	public RespDto<?> getUsersList(){
+		return new RespDto<>(1, "성공", usersDao.findAll());
+	}
+	
+	
+	@PostMapping("/users")
+	public RespDto<?> insert(JoinDto joinDto) {
+		usersDao.insert(joinDto);
+		return new RespDto<>(1, "회원가입완료", null);
+		 // 201번 - INSERT 됨
+	}
+	
+	@PutMapping("/users/{id}")
+	public RespDto<?> update(@PathVariable Integer id,UpdateDto updateDto){
+		// 1번 : id로 select 하자 ( 영속화 )
+		Users usersPS = usersDao.findById(id);
+		
+		// 2번 : 변경 
+		usersPS.전체수정(updateDto);
+	
+		// 3번 : 영속화된 오브젝트로 update치기 
+		usersDao.update(usersPS);
+		return new RespDto<>(1,"회원수정완료",null);
+	}
+	@PutMapping("/users/{id}/password")
+	public RespDto<?> updatePassword(@PathVariable Integer id, String password){
+		// 1번 영속화 
+		Users usersPS = usersDao.findById(id);
+		
+		
+		// 2번 변경
+		usersPS.패스워드수정(password);
+		
+		// 3번 전체 업데이트
+		usersDao.update(usersPS);
+		return new RespDto<>(1, "회원패스워드 수정완료",null);
+	}
+	
+}
